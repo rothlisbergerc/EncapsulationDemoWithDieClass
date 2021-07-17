e420423ed2d05b9a14d975313a8397f0a0f046a7
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-17 08:30:55 -0700

    Created Die class

---
+++ EncapsulationDemoWithDieClass/Die.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDemoWithDieClass
+{
+    /// <summary>
+    /// Represents a single six-sided game die.
+    /// </summary>
+    class Die
+    {
+
+    }
+}
